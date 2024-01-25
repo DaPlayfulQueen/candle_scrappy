0e3a4bb7abccb8a95d7fe1ef8fa2d1b6f7cb6ccb
kunstkennerin <rando@gmail.com>
2024-01-25 17:26:55 +0200

    further fix to mongo

--- mongo/init.js
+++ mongo/init.js
@@ -1,7 +1,9 @@
-var username = process.env.MONGO_USERNAME || "root";
-var password = process.env.MONGO_PASSWORD || "password";
-var database = process.env.MONGO_DATABASE || "candles";
-var collection = process.env.MONGO_COLLECTION || "candles";
+var username = process.env.MONGO_INITDB_ROOT_USERNAME || "root";
+var password = process.env.MONGO_INITDB_ROOT_PASSWORD || "password";
+var database = process.env.MONGO_INITDB_DATABASE || "candles";
+var collection = process.env.MONGO_INITDB_COLLECTION || "candles";
+
+db.auth(username, password);
 
 db.createUser({
     user: username,
