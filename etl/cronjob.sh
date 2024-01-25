021ea59975c0e53b3b6c785b1e02ae908b76740c
mako <gitgit@mail.l>
2024-01-25 22:13:40 +0000

    fixed crontab in etl

--- etl/cronjob.sh
+++ etl/cronjob.sh
@@ -1,2 +1,2 @@
-echo "15 * * * * /usr/local/bin/python /app/etl.py" > /etc/crontabs/root && \
+echo "30 * * * * /usr/local/bin/python /app/etl.py" > /etc/crontabs/root && \
     cat /etc/crontabs/root
