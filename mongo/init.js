09e2e4434bf1ab55a0cc962a16877bddc2522db1
kunstkennerin <rando@gmail.com>
2024-01-25 15:00:05 +0200

    mongo config

---
+++ mongo/init.js
@@ -0,0 +1,12 @@
+var username = process.env.MONGO_INITDB_ROOT_USERNAME || "root";
+var password = process.env.MONGO_INITDB_ROOT_PASSWORD || "password";
+var database = process.env.MONGO_INITDB_DATABASE || "candles";
+var collection = process.env.MONGO_INITDB_COLLECTION || "candles";
+
+db.createUser({
+    user: username,
+    pwd: password,
+    roles: [{ role: "readWrite", db: database }]
+});
+
+db.createCollection(collection);
